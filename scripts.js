3eeb09045e7a25250ed8d93a40fa946669567029
gatoledo1 <gatoledo1@gmail.com>
2021-07-02 18:05:00 -0300

    Criação serviço PWA

--- scripts.js
+++ scripts.js
@@ -174,4 +174,12 @@ const handleFormSubmit = event => {
 form.addEventListener("submit", handleFormSubmit)
 
 
-
+/* ======== Chamada PWA ========= */
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("./serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })
+}
